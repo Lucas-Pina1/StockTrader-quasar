312eef109440196feb6fb151a9e3d85c3512f5ef
Lucas <lucas.pina@prodepa.pa.gov.br>
2022-07-20 11:56:27 -0300

    first commit~

---
+++ src/components/header/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-header class="q-pa-sm bg-grey-3" elevated>
+    <q-toolbar>
+      <q-toolbar-title>
+        <span class="text-dark">Stock</span>
+        <span class="text-weight-light text-dark">Trader</span>
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-layout view="lHh Lpr lFf" container style="height: 400px">
+    <QHeader/>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import QHeader from "../components/header/Header.vue";
+
+export default {
+  components: {
+    QHeader,
+  },
+  name: "MainLayout",
+  data() {
+    return {};
+  },
+};
+</script>
