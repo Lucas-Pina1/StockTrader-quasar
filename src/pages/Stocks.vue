1c4d0873ca45d290483a551320afef39d188a1e0
Lucas <lucas.pina@prodepa.pa.gov.br>
2022-07-25 12:11:38 -0300

    inicio da area de ações(botão)

--- src/pages/Stocks.vue
+++ src/pages/Stocks.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="q-pa-md row items-start q-gutter-md">
-    <q-card class="my-card bg-green-4 text-white">
-      <q-card-section class="col-auto">
-        <div class="text-h4">
-          <strong>NOME </strong><small>(Preço: PREÇO)</small>
-        </div>
-      </q-card-section>
-    </q-card>
-    <q-card>
-      <q-card-section class="col-auto">
-        <q-input
-          type="number"
-          filled
-          style="max-width: 200px"
-        />
-      </q-card-section>
-    </q-card>
+    <q-form>
+      <q-card class="my-card bg-green-4 text-white">
+        <q-card-section class="col-auto">
+          <div class="text-h4">
+            <strong>NOME </strong><small>(Preço: PREÇO)</small>
+          </div>
+        </q-card-section>
+      </q-card>
+      <q-card>
+        <q-card-section class="col-auto">
+          <q-input type="number" filled style="max-width: 200px" />
+          <q-btn label="Comprar" type="submit" color="green-3"/>
+        </q-card-section>
+      </q-card>
+    </q-form>
   </div>
 </template>
 
