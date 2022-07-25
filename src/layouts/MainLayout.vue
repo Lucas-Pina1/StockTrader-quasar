eb628cbb1a6fa3dcf7c12cf9a03ce422bff8bfcd
Lucas <lucas.pina@prodepa.pa.gov.br>
2022-07-25 12:00:43 -0300

    inicio cards da area de ações

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,40 @@
 <template>
   <q-layout view="hhh Lpr fff">
-    <QHeader/>
+    <q-header class="bg-green-1 fixed-top" elevated>
+      <div class="q-pa-md">
+        <q-toolbar>
+          <q-btn flat to="/home">
+            <span class="text-dark">Stock</span>
+            <span class="text-weight-light text-green-7">Trader</span>
+          </q-btn>
+
+          <div class="row q-ml-sm">
+            <q-btn flat class="text-dark" to="/home">Inicio</q-btn>
+            <q-btn flat class="text-dark" to="/portfolio">portfólio</q-btn>
+            <q-btn flat class="text-dark" to="/stocks">Ações</q-btn>
+          </div>
+
+          <q-space />
+
+          <div class="row">
+            <q-btn flat class="text-dark">Finalizar Dia</q-btn>
+            <q-btn flat class="text-dark" label="Salvar & Carregar">
+              <q-menu fit>
+                <q-list style="min-width: 100px">
+                  <q-item clickable v-close-popup>
+                    <q-item-section>Salvar Dados</q-item-section>
+                  </q-item>
+                  <q-item clickable v-close-popup>
+                    <q-item-section>Carregar Dados</q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </q-btn>
+          </div>
+        </q-toolbar>
+      </div>
+    </q-header>
+
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -8,12 +42,7 @@
 </template>
 
 <script>
-import QHeader from "../components/header/Header.vue";
-
 export default {
-  components: {
-    QHeader,
-  },
   name: "MainLayout",
   data() {
     return {};
--- src/pages/Stocks.vue
+++ src/pages/Stocks.vue
@@ -1,13 +1,30 @@
 <template>
-  <h1>Stocks</h1>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card bg-green-4 text-white">
+      <q-card-section class="col-auto">
+        <div class="text-h4">
+          <strong>NOME </strong><small>(Preço: PREÇO)</small>
+        </div>
+      </q-card-section>
+    </q-card>
+    <q-card>
+      <q-card-section class="col-auto">
+        <q-input
+          type="number"
+          filled
+          style="max-width: 200px"
+        />
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
 </style>
