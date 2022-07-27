420c7aa2f123e290875e7b9589c81561cf2e7ebd
Lucas <lucas.pina@prodepa.pa.gov.br>
2022-07-27 12:05:29 -0300

    ajustes layout e component stock

--- src/components/stock/Stock.vue
+++ src/components/stock/Stock.vue
@@ -1,13 +1,43 @@
 <template>
-  <h1>Ação</h1>
+  <div class="q-pa-md">
+    <div class="row">
+        <q-card class="col-xs-12 col-sm-6 col-md-4 my-card bg-green-4 text-white">
+          <q-card-section>
+            <div class="text-h5 row">
+              <div class="col"><strong>NOME </strong></div>
+              <div class="col"><small>(Preço: PREÇO)</small></div>
+            </div>
+          </q-card-section>
+
+          <q-card-section>
+            <q-input
+              dense
+              class="bg-grey-1"
+              type="number"
+              min="0"
+              filled
+              style="max-width: 200px"
+              placeholder="0"
+            />
+          </q-card-section>
+
+          <q-separator dark />
+
+          <q-card-actions align="right" class="bg-grey-1">
+            <q-btn label="Comprar" type="submit" color="green-4"></q-btn>
+          </q-card-actions>
+        </q-card>
+
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
 </style>
--- src/pages/Stocks.vue
+++ src/pages/Stocks.vue
@@ -1,42 +1,31 @@
 <template>
   <q-page>
-    <div class="q-pa-md row items-start q-gutter-md">
-      <q-card class="my-card bg-green-4 text-white">
-        <q-card-section>
-          <div class="text-h5 row">
-            <div class="col"><strong>NOME </strong></div>
-            <div class="col"><small>(Preço: PREÇO)</small></div>
-          </div>
-        </q-card-section>
-
-        <q-card-section>
-          <q-input
-            dense
-            class="bg-grey-1"
-            type="number"
-            min="0"
-            filled
-            style="max-width: 200px"
-            placeholder="0"
-          />
-        </q-card-section>
-
-        <q-separator dark />
-
-        <q-card-actions align="right" class="bg-grey-1">
-          <q-btn label="Comprar" type="submit" color="green-4"></q-btn>
-        </q-card-actions>
-      </q-card>
-    </div>
+    <q-layout class="row">
+      <Stock v-for="stock in stocks" :key="stock.id"></Stock>
+    </q-layout>
   </q-page>
 </template>
 
 <script>
-export default {};
+import Stock from '../components/stock/Stock.vue'
+
+export default {
+  components: {
+    Stock,
+  },
+  data() {
+    return {
+      stocks: [
+        { id:1, name: 'BMW', price: 150 },
+        { id:2, name: 'Google', price: 200 },
+        { id:3, name: 'Apple', price: 250 },
+        { id:4, name: 'Twitter', price: 83 },
+      ]
+    }
+  }
+};
 </script>
 
-<style lang="sass" scoped>
-.my-card
-  width: 100%
-  max-width: 250px
+<style>
+
 </style>
